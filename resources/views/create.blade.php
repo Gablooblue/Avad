9bde35cee665af1458bbaf74a84e26d4ef15b0f1
gablooblue <gabrielhenry.lopez@gmail.com>
2017-02-20 21:22:18 +0800

    Revert "next to last commit"
    This reverts commit a1cc70df8bc2c757bd44cf9302a8edd459d17c74.

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -96,6 +96,10 @@
                                 @endif
                             </div>
 			</div>
+			<div class = "form-group">
+				<div class ="col-md-6 col-md-offset-4">
+					<input type="file" name="picture"></input>
+				</div>
 			</div>
 			{{ csrf_field() }}
 			<div class="form-group">
